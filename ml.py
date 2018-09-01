4ee96a7550cc9b6bc490c021ef416ac3880dcf15
thealphadollar <shivam.cs.iit.kgp@gmail.com>
2018-09-02 01:30:39 +0530

    Learn categorisation of data

--- ml.py
+++ ml.py
@@ -1,7 +1,7 @@
 import numpy as np
 import pandas as pnd
 import matplotlib.pyplot as plt
-from sklearn.preprocessing import Imputer
+from sklearn.preprocessing import Imputer, LabelEncoder, OneHotEncoder
 
 
 # importing the dataset
@@ -15,5 +15,19 @@ dependent = data_set.iloc[:, 3].values
 imputer = Imputer(missing_values="NaN", verbose=1, axis=0, copy=False)
 imputer = imputer.fit(independent[:, 1:3])
 independent[:, 1:3] = imputer.transform(independent[:, 1:3])
+# print(independent)
 
-print(independent)
+labelencoder_independent = LabelEncoder()
+labelencoder_independent.fit(independent[:, 0])
+independent[:, 0] = labelencoder_independent.transform(independent[:, 0])
+# print(independent)
+
+hotencorder_independent = OneHotEncoder(categorical_features=[0])
+hotencorder_independent.fit(independent)
+independent = hotencorder_independent.transform(independent).toarray()
+# print(independent)
+
+labelencoder_dependent = LabelEncoder()
+labelencoder_dependent.fit(dependent[:])
+dependent[:] = labelencoder_dependent.transform(dependent[:])
+print(dependent)
